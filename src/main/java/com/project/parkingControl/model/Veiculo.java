03e5f0ee2c053382f1359d77120c6e7a7653c5b7
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-10-10 14:35:43 -0300

    Criada classe modelo Estacionamento e realizada modificação no id do veículo, atribuindo a placa

--- src/main/java/com/project/parkingControl/model/Veiculo.java
+++ src/main/java/com/project/parkingControl/model/Veiculo.java
@@ -1,10 +1,59 @@
 package com.project.parkingControl.model;
 
+import javax.persistence.*;
+
 public class Veiculo
 {
-    private int id;
+    @Id
+    @Column(unique=true)
     private String placa;
+    @Column
     private String modelo;
+    @Column
     private String marca;
+    @Column
     private String cor;
+    @ManyToOne
+    @JoinColumn(name = "proprietario_fk")
+    private Proprietario proprietario;
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Proprietario getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Proprietario proprietario) {
+        this.proprietario = proprietario;
+    }
 }
