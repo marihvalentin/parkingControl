8218e196600c2bdcf0127d36af3e14d56f12be0e
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-10-10 14:01:58 -0300

    Implementada a classe modelo do Veículo

---
+++ src/main/java/com/project/parkingControl/model/Proprietario.java
@@ -0,0 +1,4 @@
+package com.project.parkingControl.model;
+
+public class Proprietario {
+}
---
+++ src/main/java/com/project/parkingControl/model/Veiculo.java
@@ -0,0 +1,10 @@
+package com.project.parkingControl.model;
+
+public class Veiculo
+{
+    private int id;
+    private String placa;
+    private String modelo;
+    private String marca;
+    private String cor;
+}
