8bd2c83f1528358c69c6a8132470873cfc66e4d6
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-10-10 14:41:00 -0300

    Adicionada a fk do veículo na classe modelo Estacionamento

--- src/main/java/com/project/parkingControl/model/Estacionamento.java
+++ src/main/java/com/project/parkingControl/model/Estacionamento.java
@@ -1,4 +1,70 @@
 package com.project.parkingControl.model;
 
-public class Estacionamento {
+import javax.persistence.*;
+import java.util.Date;
+
+public class Estacionamento
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private Date entrada;
+    @Column
+    private String saida;
+    @Column
+    private String tempoPermanencia;
+    @Column
+    private String pagamento;
+    @ManyToOne
+    @JoinColumn(name = "veiculo_fk")
+    private Veiculo veiculo;
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(Date entrada) {
+        this.entrada = entrada;
+    }
+
+    public String getSaida() {
+        return saida;
+    }
+
+    public void setSaida(String saida) {
+        this.saida = saida;
+    }
+
+    public String getTempoPermanencia() {
+        return tempoPermanencia;
+    }
+
+    public void setTempoPermanencia(String tempoPermanencia) {
+        this.tempoPermanencia = tempoPermanencia;
+    }
+
+    public String getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(String pagamento) {
+        this.pagamento = pagamento;
+    }
 }
